f3eac1fddd502bca28b2918ee788113543c66b1c
Lukas Burk <lukas@quantenbrot.de>
2024-08-12 11:02:03 +0200

    Conditionally export null default operator

---
+++ R/op-null-default.R
@@ -0,0 +1,15 @@
+#' Default value for `NULL`
+#'
+#' A backport of `%||%` available in R versions from 4.4.0.
+#'
+#' @param x,y If `x` is NULL or length 0, will return `y`; otherwise returns `x`.
+#' @rawNamespace if (getRversion() < "4.3.0") export(`%||%`)
+#' @name op-null-default
+#' @examples
+#' 1 %||% 2
+#' NULL %||% 2
+#' character(0) %|0|% ""
+#' list() %|0|% ""
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
