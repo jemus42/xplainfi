bf3dbbb34632c1e2b0351ddfd9ddc981b258a272
Lukas Burk <lukas@quantenbrot.de>
2025-06-15 02:45:23 +0200

    Attempt to silence foreach warning

--- R/zzz.R
+++ R/zzz.R
@@ -1,2 +1,8 @@
 .onLoad = function(libname, pkgname) {
+  # Register sequential backend to suppress foreach warnings when no parallel backend is set
+  if (requireNamespace("foreach", quietly = TRUE)) {
+    if (!foreach::getDoParRegistered()) {
+      foreach::registerDoSEQ()
+    }
+  }
 }
