dca0b4dc122331c4420d23c10ccdcbcdc3813a81
Lukas Burk <lukas@quantenbrot.de>
2025-08-07 14:55:35 +0200

    Cleanup

--- R/combine.R
+++ R/combine.R
@@ -1,31 +1,32 @@
-#' Combine two FeatureImportanceMethod objects
-#'
-#' @param x,y `([FeatureImportanceMethod])` Objects to combine. Must have computed scores.
-#' @param ... (any) Ignored.
-#' @return New object of the same subclass as `x` and `y`.
-#' @export
-#'
-#' @examplesIf requireNamespace("ranger", quietly = TRUE) && requireNamespace("mlr3learners", quietly = TRUE)
-#' library(mlr3)
-#' task = tgen("2dnormals")$generate(n = 100)
-#'
-#' pfi1 = PFI$new(
-#'   task = task,
-#'   learner = lrn("classif.ranger", num.trees = 50, predict_type = "prob"),
-#'   measure = msr("classif.ce"),
-#'   features = "x1"
-#' )
-#' pfi1$compute()
-#'
-#' pfi2 = PFI$new(
-#'   task = task,
-#'   learner = lrn("classif.ranger", num.trees = 50, predict_type = "prob"),
-#'   measure = msr("classif.ce"),
-#'   features = "x2"
-#' )
-#' pfi2$compute()
-#'
-#' combined = c(pfi1, pfi2)
-c.FeatureImportanceMethod = function(x, y, ...) {
-  x$combine(y)
-}
+# Deactivated until the API is more stable
+# #' Combine two FeatureImportanceMethod objects
+# #'
+# #' @param x,y `([FeatureImportanceMethod])` Objects to combine. Must have computed scores.
+# #' @param ... (any) Ignored.
+# #' @return New object of the same subclass as `x` and `y`.
+# #' @export
+# #'
+# #' @examplesIf requireNamespace("ranger", quietly = TRUE) && requireNamespace("mlr3learners", quietly = TRUE)
+# #' library(mlr3)
+# #' task = tgen("2dnormals")$generate(n = 100)
+# #'
+# #' pfi1 = PFI$new(
+# #'   task = task,
+# #'   learner = lrn("classif.ranger", num.trees = 50, predict_type = "prob"),
+# #'   measure = msr("classif.ce"),
+# #'   features = "x1"
+# #' )
+# #' pfi1$compute()
+# #'
+# #' pfi2 = PFI$new(
+# #'   task = task,
+# #'   learner = lrn("classif.ranger", num.trees = 50, predict_type = "prob"),
+# #'   measure = msr("classif.ce"),
+# #'   features = "x2"
+# #' )
+# #' pfi2$compute()
+# #'
+# #' combined = c(pfi1, pfi2)
+# c.FeatureImportanceMethod = function(x, y, ...) {
+#   x$combine(y)
+# }
